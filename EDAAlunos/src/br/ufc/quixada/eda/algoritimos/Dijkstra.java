07345fc168ed64ff5709da5d467a9408ff9c88d7
miche <miche@172.18.103.132>
2017-04-24 21:30:11 -0300

    Dijkstra

--- EDAAlunos/src/br/ufc/quixada/eda/algoritimos/Dijkstra.java
+++ EDAAlunos/src/br/ufc/quixada/eda/algoritimos/Dijkstra.java
@@ -1,5 +1,8 @@
 package br.ufc.quixada.eda.algoritimos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.ufc.quixada.eda.grafo.Aresta;
 import br.ufc.quixada.eda.grafo.Grafo;
 import br.ufc.quixada.eda.grafo.ListaAdjacencia;
@@ -8,12 +11,19 @@ import br.ufc.quixada.eda.listaprioridades.HeapMinimo;
 public class Dijkstra {
 	private int origem;
 	private int ant[] = null;
+	private int d[] = null;
+	private int vertices[] = null;
 	
 	public void executar(Grafo g){
+		int x = g.getQtdVertice();
+		d = new int[x];
+		ant = new int [x];
+		vertices =  new int[x];
 		//Inicializao Integer.MAX_VALUE
+		inicializacao(x, origem);
 		
 		HeapMinimo Q = new HeapMinimo(g.getQtdVertice());
-		Q.contruir(null);
+		Q.contruir(vertices);
 		
 		while(!Q.vazia()){
 			int u = Q.remove();
@@ -22,25 +32,26 @@ public class Dijkstra {
 			while(adj.hasNext()){
 				Aresta a = adj.next();
 				int v = (a.getU() == u ? a.getV() : a.getU());
-				
-				//Relaxamento(u, v)
+				relaxamento(adj, u, v);
 			}
 		}
 	}
 	
-	public void inicializacao(int u, int s){
-	//	for u - v{
-			//ant[u] = -1;
-			//d[v] = Integer.MAX_VALUE;
-		//}
-		//d[s] = 0;
-		//ant[s] = s;
+	public void inicializacao(int v, int s){
+		for (int u = 0; u < v; u++){
+			ant[u] = -1;
+			d[u] = Integer.MAX_VALUE;
+			vertices[u] = u;
+		}
+		d[s] = 0;
+		ant[s] = s;
 	}
 	
-	public void relaxamento(int u, int v){
-	/*	if(d[v] > w(u,v) + d[u]){
-		ant[v] = u;
-			d[v] = w(u,v) + d[u];
+	public void relaxamento(ListaAdjacencia adj, int u, int v){
+		if(d[v] > (d[u] + adj.next().getPeso())){
+			ant[v] = u;
+			d[v] = adj.next().getPeso() + d[u];
 		}
-*/	}
+		adj.next();
+	}
 }
